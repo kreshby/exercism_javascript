afb05010928fd9d790c1a5d94137d2b7fdb5ee29
Vladimir Kukresh <kreshsoftware@gmail.com>
2020-12-14 20:44:27 +0300

    fix: new solving according for tutor advice

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -3,11 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = (name = 'you') => {
+export const twoFer = (name = "you") => {
   let massage;
 
-  if (typeof name === "string" && name.trim()) {
-    massage = `One for ${name}, one for me.`;
-  } 
+  massage = `One for ${name}, one for me.`;
+
   return massage;
 };
