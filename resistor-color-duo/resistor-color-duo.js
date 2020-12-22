e7cb750a6d5106a2d1ca4e9bf237d84a2eae02d2
Vladimir Kukresh <kreshsoftware@gmail.com>
2020-12-22 18:30:29 +0300

    fix: bugs

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -2,22 +2,26 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const decodedValue = (nameColor1, nameColor2) => {
-  //throw new Error('Remove this statement and implement this function');
-  let twoDigitNumber;
+"use strict";
+var twoDigitNumber;
+export const decodedValue = (nameColor) => {
   let firstDigitNumber;
   let secondDigitNumber;
-  if (0 <= nameColor1 && nameColor1 <= 9) {
-    let firstDigitNumber = COLORS.indexOf(nameColor1);
-  }
-  if (0 <= nameColor2 && nameColor2 <= 9) {
-    let secondDigitNumber = String(COLORS.indexOf(nameColor2));
-  }
+  let nameColorOne;
+  let nameColorTwo;
 
-  twoDigitNumber = String(firstDigitNumber + secondDigitNumber);
+  nameColorOne = String(nameColor[0]).toLowerCase();
+  nameColorTwo = String(nameColor[1]).toLowerCase();
 
-  return twoDigitNumber;
+  if (COLORS.includes(nameColorOne, 0) && COLORS.includes(nameColorTwo, 0)) {
+    firstDigitNumber = COLORS.indexOf(nameColorOne);
+
+    secondDigitNumber = COLORS.indexOf(nameColorTwo);
+
+    twoDigitNumber = String(firstDigitNumber) + String(secondDigitNumber);
+
+    return Number(twoDigitNumber);
+  }
 };
 
 export const COLORS = [
