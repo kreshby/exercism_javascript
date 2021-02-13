e5b8999fe81abbdb6e775a6062bd23fd69f7b5b1
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-02-13 15:03:46 +0300

    feat: remake with array helper method

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -2,33 +2,8 @@
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-"use strict";
-var twoDigitNumber;
-export const decodedValue = (nameColor) => {
-  let result;
-  console.log(nameColor[0], nameColor[1]);
+//"use strict";
 
-  result = takeIndex(nameColor);
-
-  return Number(result.join(""));
-};
-function takeIndex(twoColors) {
-  let nameColorOne;
-  let nameColorTwo;
-  let firstDigitNumber;
-  let secondDigitNumber;
-
-  nameColorOne = String(twoColors[0]).toLowerCase();
-  nameColorTwo = String(twoColors[1]).toLowerCase();
-
-  if (COLORS.includes(nameColorOne, 0) && COLORS.includes(nameColorTwo, 0)) {
-    firstDigitNumber = COLORS.indexOf(nameColorOne);
-
-    secondDigitNumber = COLORS.indexOf(nameColorTwo);
-  }
-
-  return [firstDigitNumber, secondDigitNumber];
-}
 export const COLORS = [
   "black",
   "brown",
@@ -41,3 +16,9 @@ export const COLORS = [
   "grey",
   "white",
 ];
+let twoColors;
+
+export const decodedValue = (twoColors) =>
+  Number(
+    twoColors.map((element) => COLORS.indexOf(element.toLowerCase())).join("")
+  );
