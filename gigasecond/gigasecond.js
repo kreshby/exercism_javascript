9ef2533392dacd44bcf1457c9a6d09204d708405
Vladimir Kukresh <kreshsoftware@gmail.com>
2021-02-23 22:45:36 +0300

    feat: create the function which given a moment, determine the moment that would be after a gigasecond has passed

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  let gigaSec = 1000000000 * 1000; // seconds to milliseconds
+
+  return new Date(Date.parse(date) + gigaSec);
 };
